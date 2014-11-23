604b609a3bdb9735a1be1a3a7667dcc9190982d8
renan-ranelli <renanranelli@gmail.com>
2014-11-23 01:25:48 -0200

    Add remote installation script

---
+++ ubuntu_install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -ev
+
+# Install git of course
+sudo apt-get install -y git
+
+# Prompt user to add ssh-key to github account. This is needed for code-base cloning
+
+if [ ! -f ~/.ssh/id_rsa.pub ]; then
+    cat /dev/zero | ssh-keygen -q -N ""
+
+    echo "Add this ssh key to your github account!"
+    cat ~/.ssh/id_rsa.pub
+    echo "Press [Enter] to continue..." && read
+fi
+
+
+git clone git@github.com:rranelli/linuxsetup.git
+
+cd linuxsetup
+
+make
