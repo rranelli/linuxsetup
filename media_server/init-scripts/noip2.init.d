4385a620110c0ea8709c2b5d25f740d55db7a6a3
renan-ranelli <renanranelli@gmail.com>
2015-10-16 04:57:19 -0300

    Small changes to noip2 init daemon

--- media_server/init-scripts/noip2.init.d
+++ media_server/init-scripts/noip2.init.d
@@ -27,25 +27,25 @@ test -x $DAEMON || exit 0
 
 case "$1" in
     start)
-    echo -n "Starting dynamic address update: "
-    start-stop-daemon --start --exec $DAEMON
-    echo "noip2."
-    ;;
+        echo -n "Starting dynamic address update: "
+        start-stop-daemon --start --exec $DAEMON --name $NAME
+        echo "done."
+        ;;
     stop)
-    echo -n "Shutting down dynamic address update:"
-    start-stop-daemon --stop --oknodo --retry 30 --exec $DAEMON
-    echo "noip2."
-    ;;
+        echo -n "Shutting down dynamic address update:"
+        start-stop-daemon --stop --oknodo --retry 30 --exec $DAEMON --name $NAME
+        echo "done."
+        ;;
 
     restart)
-    echo -n "Restarting dynamic address update: "
-    start-stop-daemon --stop --oknodo --retry 30 --exec $DAEMON
-    start-stop-daemon --start --exec $DAEMON
-    echo "noip2."
-    ;;
+        echo -n "Restarting dynamic address update: "
+        start-stop-daemon --stop --oknodo --retry 30 --exec $DAEMON --name $NAME
+        start-stop-daemon --start --exec $DAEMON --name $NAME
+        echo "done."
+        ;;
 
     *)
-    echo "Usage: $0 {start|stop|restart}"
-    exit 1
+        echo "Usage: $0 {start|stop|restart}"
+        exit 1
 esac
 exit 0
