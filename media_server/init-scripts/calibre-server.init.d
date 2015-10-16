f3414ef4be9eafda2cb5e11309960fe7fc391871
renan-ranelli <renanranelli@gmail.com>
2015-10-16 04:31:38 -0300

    Make calibre-server init script actually work

--- media_server/init-scripts/calibre-server.init.d
+++ media_server/init-scripts/calibre-server.init.d
@@ -1,70 +1,69 @@
-#!/usr/bin/env bash
+#!/bin/sh
 ### BEGIN INIT INFO
-# Provides:          calibre-server
-# Required-Start:    $network $local_fs $syslog
-# Required-Stop:     $local_fs
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
-# Short-Description: Calibre server with books
-# Description:
-#
+# Provides: calibre-server
+# Required-Start: $local_fs $network
+# Required-Stop: $local_fs
+# Default-Start: 2 3 4 5
+# Default-Stop: 0 1 6
+# Short-Description: calibre-server
 ### END INIT INFO
 
-DAEMON_PATH="/usr/bin/"
-DAEMON=calibre-server
-DAEMONOPTS="--with-library '/home/renan/Copy/Biblioteca Técnica/Computing/' -p 4366"
+RUN_AS='renan'
+PIDFILE='/tmp/calibre-server.pid'
+EXECUTABLE='/opt/calibre/calibre-server'
+OPTS="--with-library /home/renan/Copy/Library/Computing \
+      --url-prefix /books \
+      --port=4366
+      --pidfile=$PIDFILE
+      --daemonize
+"
 
-NAME=calibre-server
-DESC="Web interface for serving calibre content"
-PIDFILE=/tmp/$NAME.pid
-SCRIPTNAME=/etc/init.d/$NAME
+start() {
+    # matching interpreted daemons is so frikin tricky...
+    echo 'Starting calibre server...'
+    [ -f $PIDFILE ] && { echo "pidfile $PIDFILE exists. remove it first" && exit 1 ;}
+    /sbin/start-stop-daemon --start --chuid $RUN_AS \
+                            --user $RUN_AS \
+                            --pidfile ${PIDFILE} \
+                            --exec $EXECUTABLE -- $OPTS
+    echo 'done.'
+}
+
+stop() {
+    if [ -f $PIDFILE ]; then
+        echo 'Stopping calibre server...'
+        /bin/kill $(cat $PIDFILE) && rm $PIDFILE && echo 'done.'
+    else
+        echo 'Nothing to be done.'
+    fi
+}
+
+status() {
+    dbpid=$(pgrep -fu $RUN_AS 'calibre-server --with-library')
+    if [ -z "$dbpid" ]; then
+        echo "calibre-server for user $RUN_AS: not running."
+    else
+        echo "calibre-server for user $RUN_AS: running (pid $dbpid)"
+    fi
+}
 
 case "$1" in
     start)
-	printf "%-50s" "Starting $NAME..."
-	cd $DAEMON_PATH
-	echo "$DAEMON $DAEMONOPTS"
-	PID=`sudo -u renan calibre-server --with-library '/home/renan/Copy/Biblioteca Técnica/Computing/' -p 4366 > /dev/null 2>&1 & echo $!`
-	#echo "Saving PID" $PID " to " $PIDFILE
-        if [ -z $PID ]; then
-            printf "%s\n" "Fail"
-        else
-            echo $PID > $PIDFILE
-            printf "%s\n" "Ok"
-        fi
-	;;
-    status)
-        printf "%-50s" "Checking $NAME..."
-        if [ -f $PIDFILE ]; then
-            PID=`cat $PIDFILE`
-            if [ -z "`ps axf | grep ${PID} | grep -v grep`" ]; then
-                printf "%s\n" "Process dead but pidfile exists"
-            else
-                echo "Running"
-            fi
-        else
-            printf "%s\n" "Service not running"
-        fi
-	;;
+        start
+        ;;
     stop)
-        printf "%-50s" "Stopping $NAME"
-        PID=`cat $PIDFILE`
-        cd $DAEMON_PATH
-        if [ -f $PIDFILE ]; then
-            kill -HUP $PID
-            printf "%s\n" "Ok"
-            rm -f $PIDFILE
-        else
-            printf "%s\n" "pidfile not found"
-        fi
-	;;
-
-    restart)
-  	$0 stop
-  	$0 start
-	;;
-
+        stop
+        ;;
+    restart|reload|force-reload)
+        stop
+        start
+        ;;
+    status)
+        status
+        ;;
     *)
-        echo "Usage: $0 {status|start|stop|restart}"
+        echo "Usage: /etc/init.d/calibre-server {start|stop|reload|force-reload|restart|status}"
         exit 1
 esac
+
+exit 0
