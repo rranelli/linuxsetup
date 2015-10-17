3953243de0ae7fe2dae43138449a8ffd01e28178
renan-ranelli <renanranelli@gmail.com>
2015-10-17 00:49:54 -0300

    Fix calibre-server init script

--- media_server/init-scripts/calibre-server.init.d
+++ media_server/init-scripts/calibre-server.init.d
@@ -8,9 +8,10 @@
 # Short-Description: calibre-server
 ### END INIT INFO
 
+NAME="calibre-server"
 RUN_AS='renan'
 PIDFILE='/tmp/calibre-server.pid'
-EXECUTABLE='/opt/calibre/calibre-server'
+DAEMON='/opt/calibre/calibre-server'
 OPTS="--with-library /home/renan/Copy/Library/Computing \
       --url-prefix /books \
       --port=4366
@@ -19,31 +20,29 @@ OPTS="--with-library /home/renan/Copy/Library/Computing \
 "
 
 start() {
-    # matching interpreted daemons is so frikin tricky...
-    echo 'Starting calibre server...'
-    [ -f $PIDFILE ] && { echo "pidfile $PIDFILE exists. remove it first" && exit 1 ;}
-    /sbin/start-stop-daemon --start --chuid $RUN_AS \
-                            --user $RUN_AS \
+    echo -n "Starting daemon: $NAME"
+    /sbin/start-stop-daemon --start --quiet \
+                            --chuid $RUN_AS --user $RUN_AS \
                             --pidfile ${PIDFILE} \
-                            --exec $EXECUTABLE -- $OPTS
-    echo 'done.'
+                            --startas $DAEMON -- $OPTS
+    echo '.'
 }
 
 stop() {
-    if [ -f $PIDFILE ]; then
-        echo 'Stopping calibre server...'
-        /bin/kill $(cat $PIDFILE) && rm $PIDFILE && echo 'done.'
-    else
-        echo 'Nothing to be done.'
-    fi
+    echo -n "Stopping daemon: $NAME"
+    /sbin/start-stop-daemon --stop --quiet --oknodo \
+                            --user $RUN_AS \
+                            --pidfile ${PIDFILE} \
+                            --startas $DAEMON -- $OPTS
+    echo '.'
 }
 
 status() {
     dbpid=$(pgrep -fu $RUN_AS 'calibre-server --with-library')
     if [ -z "$dbpid" ]; then
-        echo "calibre-server for user $RUN_AS: not running."
+        echo "$NAME for user $RUN_AS: not running."
     else
-        echo "calibre-server for user $RUN_AS: running (pid $dbpid)"
+        echo "$NAME for user $RUN_AS: running (pid $dbpid)"
     fi
 }
 
