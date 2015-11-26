9d10ceb2cf5dced26dc9eb90e91e9970ccd875ae
renan-ranelli <renanranelli@gmail.com>
2015-11-26 20:29:11 -0200

    Add conkeror keybindings file

---
+++ dotfiles/.conkerorrc/keybindings.js
@@ -0,0 +1,5 @@
+// keybindings
+define_key(default_global_keymap, "M-k", "kill-current-buffer");
+define_key(default_global_keymap, "M-l", "switch-to-buffer");
+define_key(default_global_keymap, "M-o", "buffer-next");
+define_key(default_global_keymap, "C-l", "find-alternate-url");
