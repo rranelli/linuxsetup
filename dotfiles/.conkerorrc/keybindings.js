51a6c92c0df98a74c5fa83455b03d4ab823013ad
renan-ranelli <renanranelli@gmail.com>
2016-03-20 16:37:21 -0300

    Remove conkeror config

--- dotfiles/.conkerorrc/keybindings.js
+++
@@ -1,5 +0,0 @@
-// keybindings
-define_key(default_global_keymap, "M-k", "kill-current-buffer");
-define_key(default_global_keymap, "M-l", "switch-to-buffer");
-define_key(default_global_keymap, "M-o", "buffer-next");
-define_key(default_global_keymap, "C-l", "find-alternate-url");
