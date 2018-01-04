289c7eaf9d5c675ac176b1d94d22df6e2cf8b832
milhouse <renanranelli@gmail.com>
2018-01-04 18:11:37 -0200

    aff

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,5 @@
 #!/bin/bash -ev
-echo 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main' \
-  | sudo tee /etc/apt/sources.list.d/ansible-trusty.list
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-sudo apt-get update -qq
-sudo apt-get install -y git ansible
+sudo apt install -y git ansible dirmngr aptitude --allow-downgrades
 
 if [ ! -f ~/.ssh/id_rsa.pub ]; then
     cat /dev/zero | ssh-keygen -q -N ""
@@ -14,7 +10,7 @@ if [ ! -f ~/.ssh/id_rsa.pub ]; then
 fi
 
 mkdir -p ~/code/linuxsetup && cd ~/code/linuxsetup
-git clone git@github.com:rranelli/linuxsetup.git .
+[ -d ~/code/linuxsetup ] || git clone git@github.com:rranelli/linuxsetup.git .
 
 ansible-playbook --ask-become-pass \
                  --ask-vault-pass \
