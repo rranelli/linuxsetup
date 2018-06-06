f975c42bb002a2f0a1fa0f27ae3d9f43be41e5c4
Milhouse <renanranelli@gmail.com>
2018-06-06 12:32:20 -0300

    Add ssh-keys
    Signed-off-by: Milhouse <renanranelli@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-if [ ! -f ~/.ssh/id_rsa.pub ]; then
-    cat /dev/zero | ssh-keygen -q -N ""
-
-    echo "Add this ssh key to your github account!"
-    cat ~/.ssh/id_rsa.pub
-    echo "Press [Enter] to continue..." && read
-fi
-
 sudo apt update -qq || true
 sudo apt install -y git python-pip dirmngr aptitude --allow-downgrades
 sudo pip install ansible
