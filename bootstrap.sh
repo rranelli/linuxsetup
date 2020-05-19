9367b5ea6184941832f518456e828f31ecdd0828
Milhouse <renanranelli@gmail.com>
2020-05-19 11:51:31 -0300

    Actually better bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,8 +5,14 @@ sudo apt update -qq || true
 sudo apt install -y git python-pip dirmngr aptitude --allow-downgrades
 sudo pip install ansible
 
-[ -d linuxsetup ] || (git clone https://github.com/rranelli/linuxsetup.git)
-cd linuxsetup
+if [ -d linuxsetup ]; then
+  cd linuxsetup
+elif [ -f bootstrap.sh ]; then
+  cd .
+else
+  git clone https://github.com/rranelli/linuxsetup.git
+  cd linuxsetup
+fi
 
 ansible-playbook --ask-become-pass \
                  --ask-vault-pass \
